1dc0c1fc260ec996913742e90c58b6815a3c236b
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-07-21 16:52:48 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -1,4 +1,4 @@
-class Testsample < Formula
+class Griddb < Formula
   homepage "https://github.com/tungduong97/homebrew-tools"
   url "https://github.com/tungduong97/homebrew-tools/files/4952591/scrip.tar.gz"
   sha256 "2ca4d6ff465ee6848cc70a84f0461133edbbc765"
