b475377bc32ffd6771c50870244b8f735b4660b2
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-07-22 13:49:04 +0700

    Add files via upload

---
+++ basr.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+wget https://github.com/tungduong97/homebrew-tools/releases/download/1.0.0/griddb-macos-installer-x64-4.5.0.pkg
+sudo installer -pkg griddb-macos-installer-x64-4.5.0.pkg -target /
+
