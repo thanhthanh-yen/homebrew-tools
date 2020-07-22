8f15457562188bd2f5d7d7f9ebbec100eeb5b8f7
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-07-22 19:36:04 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -5,13 +5,6 @@ class Griddb < Formula
   homepage "https://github.com/tungduong97/homebrew-tools"
   url "https://github.com/tungduong97/homebrew-tools/files/4959887/griddbduong.tar.gz"
   #sha256 "0189411e5d20f296301c589e23a56dfab4994342fad158dc62227e4126253c77"
-  system "wget https://github.com/tungduong97/homebrew-tools/files/4959887/griddbduong.tar.gz ~/Downloads"
-  system "cd ~/Download "
-  system "tar xvf griddbduong.tar.gz"
-  system "cd griddbduong/client/c/"
-  system "./bootstrap.sh"
-  system "./configure"
-  system "make"
   version "1.0.0"
   # Homebrew requires tests.
   def install
