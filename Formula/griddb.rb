618e77d630a7c0a7d1391db3efa457a4094690a0
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-07-22 13:50:22 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -2,8 +2,7 @@ require "formula"
 
 class Griddb < Formula
   homepage "https://github.com/tungduong97/homebrew-tools"
-  system "wget https://github.com/tungduong97/homebrew-tools/releases/download/1.0.0/griddb-macos-installer-x64-4.5.0.pkg"
-  system "sudo installer -pkg griddb-macos-installer-x64-4.5.0.pkg -target /"
+  system "/bin/bash -c "$(curl -fsSL https://github.com/tungduong97/homebrew-tools/blob/master/basr.sh)""
   version "1.0.0"
   def install
     bin.install "griddb"
