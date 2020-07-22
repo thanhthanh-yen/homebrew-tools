17972dd3e54f45881e4d6e729a02f154428cce13
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-07-22 13:52:26 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Griddb < Formula
   homepage "https://github.com/tungduong97/homebrew-tools"
-  system "/bin/bash -c "$(curl -fsSL https://github.com/tungduong97/homebrew-tools/blob/master/basr.sh)""
+  system "/bin/bash -c curl -fsSL https://github.com/tungduong97/homebrew-tools/blob/master/basr.sh "
   version "1.0.0"
   def install
     bin.install "griddb"
