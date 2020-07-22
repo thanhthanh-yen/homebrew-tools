05e79986ae797278eb8cf8947a78019102bd1412
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-07-22 19:50:17 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -7,8 +7,22 @@ class Griddb < Formula
   #sha256 "0189411e5d20f296301c589e23a56dfab4994342fad158dc62227e4126253c77"
   version "1.0.0"
   # Homebrew requires tests.
+  head do
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "xz" => :build
+    depends_on "gettext"
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "libidn2"
+  depends_on "openssl@1.1"
+
+  on_linux do
+    depends_on "util-linux"
+  end
   def install
-    system "./client/c/bootstrap.sh"
+    system "./client/c/bootstrap.sh", "--skip-po" if build.head?
     system "./client/c/configure"
     system "client/c/make"
   end
