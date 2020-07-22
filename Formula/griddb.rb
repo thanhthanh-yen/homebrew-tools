eb54d34f381226a8acf5ef6713a8fece77c92725
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-07-22 14:15:50 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -6,7 +6,7 @@ class Griddb < Formula
   version "1.0.0"
   # Homebrew requires tests.
   def install
-    system "installer -pkg griddb-macos-installer-x64-4.5.0.pkg -target /"
+    system "/usr/bin/sudo installer -pkg griddb-macos-installer-x64-4.5.0.pkg -target /"
     bin.install "griddb"
   end
 
