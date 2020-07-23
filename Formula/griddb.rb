f4e79f7eb8d261f95702e1c9ff4d13fe4ff268cb
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-07-23 08:41:44 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -3,7 +3,7 @@ require "formula"
 class Griddb < Formula
   desc "Internet file retriever"
   homepage "https://github.com/tungduong97/homebrew-tools"
-  url "https://github.com/tungduong97/homebrew-tools/files/4959887/griddbduong.tar.gz"
+  url "https://github.com/tungduong97/homebrew-tools/files/4963482/duongduong.tar.gz"
   #sha256 "0189411e5d20f296301c589e23a56dfab4994342fad158dc62227e4126253c77"
   version "1.0.0"
   # Homebrew requires tests.
