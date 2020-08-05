9bc7be0e3c6f74709eb88c581cc1c8151f0e438d
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-08-05 15:57:26 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -6,22 +6,13 @@ class Griddb < Formula
   url "https://github.com/tungduong97/homebrew-tools/files/5027062/griddb-c-client.tar.gz"
   sha256 "c2b75496198bd9c6505c942556c30f4839e930270d5064b9fff3a9ac0abce93e"
   version "1.0.0"
-  head do
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-    depends_on "xz" => :build
-    depends_on "gettext"
-  end
-    depends_on "pkg-config" => :build
-    depends_on "libidn2"
-    depends_on "openssl@1.1"
-  on_linux do
-    depends_on "util-linux"
-  end
   
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
   def install
-    system "cd client/c; ./bootstrap.sh; ./configure; make", "--skip-po" if build.head?
+    system "cd client/c; ./bootstrap.sh; ./configure; make"
   end
 
   test do
