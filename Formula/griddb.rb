04d8d681b033f75b384e86ad4bb3c9d4b65f4dfe
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-08-05 15:37:45 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -6,22 +6,7 @@ class Griddb < Formula
   url "https://github.com/tungduong97/homebrew-tools/files/5027062/griddb-c-client.tar.gz"
   #sha256 "bc67ce84751129222f9c6fe7c27116f392ac33e48c4c123a3e835caed201f444"
   version "1.0.0"
-  # Homebrew requires tests.
-  head do
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-    depends_on "xz" => :build
-    depends_on "gettext"
-  end
 
-  depends_on "pkg-config" => :build
-  depends_on "libidn2"
-  depends_on "openssl@1.1"
-
-  on_linux do
-    depends_on "util-linux"
-  end
   def install
     system "wget https://github.com/tungduong97/homebrew-tools/files/5027062/griddb-c-client.tar.gz"
     system "tar vxfz griddb-c-client.tar.gz"
