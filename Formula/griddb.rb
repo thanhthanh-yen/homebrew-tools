0dc3af5a1cc2e68db80732cbe943aec6afec6ab0
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-08-07 18:10:54 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -10,8 +10,10 @@ class Griddb < Formula
   depends_on "autoconf"
   depends_on "automake"
   depends_on "libtool"
+  depends_on "libtoolize"
   depends_on "llvm"
-  depends_on "cmake"
+  depends_on "glibtoolize"
+  depends_on "glibtool"
   
   def install
     system "cd client/c; ./bootstrap.sh; ./configure; make"
