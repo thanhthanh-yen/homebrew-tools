38c8192e0ccd5271661ad9629a2f8d52979c68ee
tungduong97 <49387566+tungduong97@users.noreply.github.com>
2020-09-08 17:30:51 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -13,10 +13,7 @@ class Griddb < Formula
   depends_on "llvm"
   
   def install
-    system "cd", "client/c"
-    system "./bootstrap.sh"
-    system "./configure", "--prefix=#{prefix}"
-    system "make", "install"
+    system "cd client/c; ./bootstrap.sh; ./configure --prefix=#{prefix} ; make install DESTDIR=/Users/$USER"
   end
   
   test do
