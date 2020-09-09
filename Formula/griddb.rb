13c949a655741a34244fbdd1757198f1809b42e1
thanhthanh-yen <57885830+thanhthanh-yen@users.noreply.github.com>
2020-09-09 15:32:10 +0700

    Update griddb.rb

--- Formula/griddb.rb
+++ Formula/griddb.rb
@@ -4,18 +4,18 @@ class Griddb < Formula
   desc "Internet file retriever"
   homepage "https://github.com/tungduong97/homebrew-tools"
   url "https://github.com/tungduong97/homebrew-tools/files/5049101/tungduong.tar.gz"
-  sha256 "236c275a317947bb4595ac50cfc52d54ebc875ab0e5cc083f0196d921af80108"
   version "1.0.0"
-  
+  sha256 "236c275a317947bb4595ac50cfc52d54ebc875ab0e5cc083f0196d921af80108"
+
   depends_on "autoconf"
   depends_on "automake"
   depends_on "libtool"
   depends_on "llvm"
-  
+
   def install
     system "cd client/c; ./bootstrap.sh; ./configure --prefix=#{prefix} ; make install"
   end
-  
+
   test do
     assert_match "griddb version 1.0.0", shell_output("#{bin}/griddb -v", 2)
   end
